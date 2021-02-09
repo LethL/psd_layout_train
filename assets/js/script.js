f95e0f41978a6021f6dd6ae6e982afdb2dec6bbd
Leth4L <carribean_08@mail.ru>
2021-02-09 16:54:59 +0300

    add js

---
+++ assets/js/script.js
@@ -0,0 +1,71 @@
+$(function() {
+
+    let header = $('#header'),
+        introH = $('#intro').innerHeight(),
+        scrollOffset = $(window).scrollTop();
+
+    
+    // Header fixed
+    checkScroll(scrollOffset);
+
+    $(window).on('scroll', function() {
+        scrollOffset = $(this).scrollTop();
+        
+        checkScroll(scrollOffset); 
+    });
+
+    function checkScroll(scrollOffset) {
+        if (scrollOffset >= introH) {
+            header.addClass('fixed');
+        } else {
+            header.removeClass('fixed');
+        }
+    }
+
+
+    // Smooth Scroll
+    $('[data-scroll]').on('click', function(event) {
+        event.preventDefault();
+
+        let $this = $(this),
+            blockId = $this.data('scroll'),
+            blockOffset = $(blockId).offset().top;
+
+        $('#nav a').removeClass('active');
+        $this.addClass('active');
+
+        $('html, body').animate({
+            scrollTop: blockOffset 
+        }, 500);
+    });
+
+
+    // Burger Menu
+    $('#nav_toggle').on('click', function(event) {
+        event.preventDefault();
+
+        $(this ).toggleClass('active');
+        $('#nav').toggleClass('active');
+    });
+
+
+    // Accordeon collapse
+    $('[data-collapse]').on('click', function(event) {
+        event.preventDefault();
+
+        let $this = $(this),
+            blockId = $this.data('collapse');
+
+        $this.toggleClass('active');
+    });
+
+
+    // Slider
+    $('[data-slider]').slick({
+        infinite: true,
+        fade: false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+
+});
